b98b1d0b53433f6b1ea64a74989c32032806662c
StellarLuminosity <k.lambertcanada@gmail.com>
2026-02-21 18:24:57 -0500

    run settings change

--- discoveryguy/src/discoveryguy/run_script.sh
+++ discoveryguy/src/discoveryguy/run_script.sh
@@ -9,7 +9,15 @@ cat "$SCRIPT_PATH"
 echo "Running the script... in sh"
 # Run it using Python.
 timeout 60 python3 "$SCRIPT_PATH" "$@"
+PYTHON_EXIT_CODE=$?
+
+# Some generated scripts still write to /work/crash.txt; recover it for standalone mode.
+if [ -f /work/crash.txt ] && [ ! -f crash.txt ]; then
+    cp /work/crash.txt crash.txt
+fi
 
 if [ -f crash.txt ] && [ $(stat -c%s crash.txt) -gt 2097152 ]; then
     truncate -s 2M crash.txt
 fi
+
+exit $PYTHON_EXIT_CODE
